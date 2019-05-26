3fcfef1694c1d1aeca0c7e96f4334985b9002c37
Samuel Molinari <samuel@molinari.me>
2019-05-26 17:57:26 +0100

    First commit

---
+++ lib/webserver_log.rb
@@ -0,0 +1,6 @@
+require "webserver_log/version"
+
+module WebserverLog
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/webserver_log/version.rb
@@ -0,0 +1,3 @@
+module WebserverLog
+  VERSION = "0.1.0"
+end
